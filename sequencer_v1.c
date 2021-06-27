942af3a76031f39b9a13daff955c6b24bbaa4d5b
Paul T Davies <paul_t_d@hotmail.com>
2021-06-27 17:02:43 +0000

    Sequence added

--- sequencer_v1.c
+++ sequencer_v1.c
@@ -13,7 +13,47 @@ unsigned char mode;
 unsigned char bits = 8;
 unsigned int speed = 5000;
 
-static void send(int fd, short val)
+int setup()
+{
+	int status = 0;
+	int fd;
+
+	fd = open("/dev/spidev0.0", O_RDWR);
+	
+	if (fd < 0)
+	{
+		perror("Failed to open device");
+		exit(1);
+	}
+
+	status = ioctl(fd, SPI_IOC_WR_MODE, &mode);
+	
+	if (status == -1)
+	{
+		perror("Failed to set SPI mode");
+		exit(1);
+	}
+
+	status = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
+	
+	if (status == -1)
+	{
+		perror("Failed to set bits per word.");
+		exit(1);
+	}
+
+	status = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+	
+	if (status == -1)
+	{
+		perror("can't set max speed hz");
+		exit(1);
+	}
+	
+	return fd;
+}
+
+void send(int fd, short val)
 {
 	int status;
     char highByte = (val >> 6) | 0b00110000;
@@ -51,62 +91,41 @@ float getControlVoltage(int noteNumber)
 	return controlVoltage;
 }
 
+int getBits(int noteNumber)
+{
+	int cvNoteNumber = noteNumber + 3;
+	int bits = 1023 * cvNoteNumber / 127;
+	return bits;
+}
+
 void getNoteName(int noteNumber)
 {
 	int noteInOctave = noteNumber % NUMER_OF_NOTES_IN_OCTAVE;
 	char * note = arrStrNotes[noteInOctave];
 	int octave = noteNumber / NUMER_OF_NOTES_IN_OCTAVE;
-	printf("Note: %s%d: ", note, octave);
+	printf("Note: %s%d, ", note, octave);
 }
 
 int main(int argc, char *argv[])
 {
-	int status = 0;
-	int fd;
-
-	fd = open("/dev/spidev0.0", O_RDWR);
+	int sequence[] = { 1, 31, 36, 43, 40, 48, 53, 52 };
+	int fd = setup();
+	int pos, bits, i = 0;
+	float cv;
 	
-	if (fd < 0)
+	for (i = 0; i < 80;  i ++)
 	{
-		perror("Failed to open device");
-		exit(1);
+		pos = i % 8;
+		getNoteName(sequence[pos]);
+		cv = getControlVoltage(sequence[pos]);
+		printf("Voltage: %f, ", cv);
+		bits = getBits(sequence[pos]);
+		printf("Bits: %i\r\n", bits);
+		send(fd, bits);
+		usleep(250000);
 	}
 
-	status = ioctl(fd, SPI_IOC_WR_MODE, &mode);
-	
-	if (status == -1)
-	{
-		perror("Failed to set SPI mode");
-		exit(1);
-	}
-
-	status = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
-	
-	if (status == -1)
-	{
-		perror("Failed to set bits per word.");
-		exit(1);
-	}
-
-	status = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
-	
-	if (status == -1)
-	{
-		perror("can't set max speed hz");
-		exit(1);
-	}
-
-	send(fd, 1023);
 	close(fd);
-	return status;
-
-	
-	getNoteName(0);
-	for (i = 0; i <= 60; i++)
-	{
-		getNoteName(i);
-		printf("%f\r\n", getControlVoltage(i));
-	}
-
+	return 0;
 }
 
